986ec3aa16d345b879d8d93bf5a7fe903558a758
Bernd Stroessenreuther <=>
2015-02-20 16:21:55 +0100

    sitemap is now generated

--- bin/build.sh
+++ bin/build.sh
@@ -6,7 +6,6 @@
 BINDIR=$(dirname $0)
 ADDIR=$BINDIR/../asciidoc
 HTMLDIR=$BINDIR/../www
-TMPDIR=$BINDIR/../tmp
 RESOURCESDIR=$BINDIR/../web-resources
 POSTPROCESSOR_INCLUDES_DIR=$BINDIR/../postprocessor-includes
 
@@ -23,18 +22,13 @@ then
     mkdir -p $HTMLDIR || exit 1
 fi
 
-if [ ! -d $TMPDIR ]
-then
-    mkdir -p $TMPDIR || exit 1
-fi
-
 echo "### working in $HTMLDIR:"
 
-#TMP_SITEMAP=$(mktemp --tmpdir=$HTMLDIR)
-#echo "   creating new sitemap in $TMP_SITEMAP"
+TMP_SITEMAP=$(mktemp --tmpdir=$HTMLDIR)
+echo "   creating new sitemap in $TMP_SITEMAP"
 
-#echo '<?xml version="1.0" encoding="UTF-8"?>' > $TMP_SITEMAP
-#echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' >> $TMP_SITEMAP
+echo '<?xml version="1.0" encoding="UTF-8"?>' > $TMP_SITEMAP
+echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' >> $TMP_SITEMAP
 
 if [ ! -z $1 ]
 then
@@ -51,17 +45,16 @@ do
     mkdir -p $HTMLDIR/$DESTDIR/
     echo "   creating $HTMLDIR/$DESTDIR/$BASENAME.html"
     asciidoc -b xhtml11 -o $HTMLDIR/$DESTDIR/$BASENAME.html $FILE
-    #$BINDIR/postprocessor.pl --file $HTMLDIR/$DESTDIR/$BASENAME.html --tmpdir $TMPDIR --includes-dir $POSTPROCESSOR_INCLUDES_DIR
-    #echo "    <url>" >> $TMP_SITEMAP
-    #echo "        <loc>http://wiki.gluga.de/$BASENAME.html</loc>" >> $TMP_SITEMAP
-    #echo "        <lastmod>$MTIME</lastmod>" >> $TMP_SITEMAP
-    #echo "    </url>" >> $TMP_SITEMAP
+    echo "    <url>" >> $TMP_SITEMAP
+    echo "        <loc>http://wiki.gluga.de/$BASENAME.html</loc>" >> $TMP_SITEMAP
+    echo "        <lastmod>$MTIME</lastmod>" >> $TMP_SITEMAP
+    echo "    </url>" >> $TMP_SITEMAP
 done
 
-#echo '</urlset>' >> $TMP_SITEMAP
-#echo "   renaming sitemap to sitemap.xml"
-#mv $TMP_SITEMAP $HTMLDIR/sitemap.xml || rm $TMP_SITEMAP
-#chmod 644 $HTMLDIR/sitemap.xml
+echo '</urlset>' >> $TMP_SITEMAP
+echo "   renaming sitemap to sitemap.xml"
+mv $TMP_SITEMAP $HTMLDIR/sitemap.xml || rm $TMP_SITEMAP
+chmod 644 $HTMLDIR/sitemap.xml
 
 echo
 echo "### syncing static files"
